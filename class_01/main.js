32603bf76b32d08dd3db6a17874a493e9631db6f
saniyaj mallik <saniyajmallik12@gmail.com>
2024-05-07 09:45:53 +0530

    class 01

---
+++ class_01/main.js
@@ -0,0 +1,39 @@
+console.log("Hey programmers")
+
+// var
+var myName;
+myName = "Saniyaj"
+
+// let
+let num;
+num = 90;
+
+// const
+
+const pi = 3.14;
+
+console.log(myName);
+console.log(num);
+console.log(pi);
+
+// scope
+let global = 50;
+
+function local() {
+    let localvar = 100;
+    console.log(localvar);
+    console.log(global);
+    
+}
+local()
+
+// code execution
+// sync
+// async
+
+
+// hoisting
+
+const hoist = 10;
+console.log(hoist);
+
